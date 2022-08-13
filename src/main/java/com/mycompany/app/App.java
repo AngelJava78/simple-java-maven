e0d5e668ecb4a841a5bfa3147a93456218cf824a
AngelJava78 <angel_java@hotmail.com>
2022-08-13 11:29:28 -0500

    Se agregó método getName

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,15 +8,21 @@ public class App
 {
 
     private final String message = "Hello World!";
+    private final String name= "Angel";
 
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println(new App().getName());
     }
 
     private final String getMessage() {
         return message;
+    }
+
+    private final String getName() {
+        return name;
     }
 
 }
