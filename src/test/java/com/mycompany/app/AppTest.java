083cc34d0a93851876a6ee4d3f07b3e7953406fd
AngelJava78 <angel_java@hotmail.com>
2022-08-14 04:46:36 -0500

    Test modificado

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -35,7 +35,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello Angel!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello Angel Javier!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello Angel!\"");
         }
